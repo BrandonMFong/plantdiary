65f717d80d3d28066352bde26b6f743be71728b0
Brando <fong.m.brandon97@gmail.com>
2023-04-06 15:27:33 -0700

    Cleaning up test

--- interface/testbench/session_tests.h
+++ interface/testbench/session_tests.h
@@ -11,20 +11,10 @@
 #include <session.h>
 #include <stdlib.h>
 
-int test_SessionUnknownArg() {
-	char * argv[] = {"exec", "session", "unknown"};
-	int result = SessionExec(sizeof(argv)/sizeof(argv[0]), argv);
-	PRINT_TEST_RESULTS(result != 0);
-	return result != 0 ? 0 : result;
-}
-
 void session_tests(int * pass, int * fail) {
 	int p = 0, f = 0;
 
 	INTRO_TEST_FUNCTION;
-
-	if (!test_SessionUnknownArg()) p++;
-	else f++;
 
 	if (pass) *pass += p;
 	if (fail) *fail += f;
